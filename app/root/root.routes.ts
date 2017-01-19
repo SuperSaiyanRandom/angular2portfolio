94daaa42ac5de5c503d731645f1c815f378b6a09
StevoOo <stevooo@mail.com>
2017-01-19 19:19:51 +0100

    bla ...

--- app/root/root.routes.ts
+++ app/root/root.routes.ts
@@ -1,10 +1,11 @@
-import { Router, RouterConfig }  from '@angular/router';
-import { HomeComponent }                from '../home/home.component';
-import { DetailComponent }              from '../detail/detail.component';
-import { ProjectsComponent }            from '../project/project.component';
-import { AboutComponent }               from '../about/about.component';
-import { ErrorComponent }               from '../error/error.component';
-import { ProjectService }               from '../shared/project.service';
+// import { Router, RouterConfig }  from '@angular/router';                    // Todo - wrong
+import { RouterModule }          from '@angular/router';                    // stevo
+// import { HomeComponent }                from '../home/home.component';
+// import { DetailComponent }              from '../detail/detail.component';
+// import { ProjectsComponent }            from '../project/project.component';
+// import { AboutComponent }               from '../about/about.component';
+// import { ErrorComponent }               from '../error/error.component';
+// import { ProjectService }               from '../shared/project.service';
 
 const routes: RouterConfig = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
